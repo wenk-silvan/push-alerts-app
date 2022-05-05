38d79882fdab4139500058378ce3eee0f11563bd
Silvan Wenk <silvan.wenk@stud.hslu.ch>
2022-05-05 09:49:29 +0200

    Add app launcher icon

--- app/src/main/java/ch/wenksi/pushalerts/util/Constants.kt
+++ app/src/main/java/ch/wenksi/pushalerts/util/Constants.kt
@@ -3,7 +3,6 @@ package ch.wenksi.pushalerts.util
 object Constants {
     const val apiTimeout: Long = 5_000
     const val apiBaseUrl = "http://10.0.2.2:5032/api/"
-//    const val apiBaseUrl = "http://147.88.62.76:80/api/"
     const val apiProjectsUri = "projects"
     const val apiTasksUri = "tasks"
     const val apiLoginUri = "login"
