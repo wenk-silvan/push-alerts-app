0187fb5a3b0cb7a3291e22f90474d68adf9d8035
Silvan Wenk <silvan.wenk@stud.hslu.ch>
2022-05-20 17:04:38 +0200

    Add real text to AboutFragment

--- app/src/main/java/ch/wenksi/pushalerts/ui/AboutFragment.kt
+++ app/src/main/java/ch/wenksi/pushalerts/ui/AboutFragment.kt
@@ -20,15 +20,33 @@ class AboutFragment : Fragment() {
     private var _binding: FragmentAboutBinding? = null
     private val binding get() = _binding!!
     private lateinit var recyclerViewAdapter: LicensesAdapter
+    private val description = "This App is part of the Push Alerts System and provides a solution to efficiently manage events or urgent tasks within a team. Tasks are created automatically through external systems and users of the according project get notified and reminded through push-notifications. Assign yourself to a task and mark it as Done or Rejected when finished."
     private val licenses = listOf(
-        License("aafsd", "adfasd", "asdfasdf"),
-        License("aafsd", "adfasd", "asdfasdf"),
-        License("aafsd", "adfasd", "asdfasdf"),
-        License("aafsd", "adfasd", "asdfasdf"),
-        License("aafsd", "adfasd", "asdfasdf"),
-        License("aafsd", "adfasd", "asdfasdf"),
-        License("aafsd", "adfasd", "asdfasdf"),
-        License("aafsd", "adfasd", "asdfasdf"),
+        License(
+            "AndroidX",
+            "Core platform to develop android apps\ndeveloper.android.com/jetpack/androidx#api_reference",
+            "Apache 2.0 License"
+        ),
+        License(
+            "Material Design 2",
+            "Design framework of Google\nmaterial.io",
+            "Apache 2.0 License"
+        ),
+        License(
+            "Square Retrofit 2",
+            "Library to create type-safe http clients for Android and Java\nsquare.github.io/retrofit/",
+            "Apache 2.0 License"
+        ),
+        License(
+            "Square OkHttp 3",
+            "Library http middlewares and mocking server\nsquare.github.io/okhttp/",
+            "Apache 2.0 License"
+        ),
+        License(
+            "FCM",
+            "Firebase Cloud Messaging is a library to communicate with Firebase's push-notification service\nfirebase.google.com/docs/cloud-messaging/",
+            "Apache 2.0 License"
+        )
     )
 
     override fun onCreateView(
@@ -41,6 +59,7 @@ class AboutFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.tvDescription.text = description
         initRecyclerView()
     }
 
